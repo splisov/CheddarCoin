c1eeb6042d1b46835e1356cdf34b89c54ffc81ad
splisov <sergey@splisov.com>
2018-07-07 19:29:38 -0400

    First working test

--- test/CheeseToken.test.js
+++ test/CheeseToken.test.js
@@ -1,12 +1,12 @@
 var CheeseToken = artifacts.require("contracts/CheeseToken.sol");
 
 contract('CheeseToken', function(accounts) {
-  it("should start with 10^18 wei", function() {
+  it("should start with 10^22 wei", function() {
     return CheeseToken.deployed().then(function(instance) {
       return instance.balanceOf(accounts[0]);
     }).then(function(balance) {
-      console.log(balance);
-      assert.equal(balance, 10 ** 18);
+      console.log(balance.toNumber());
+      assert.equal(balance, 10 ** 22);
     });
   });
 });
