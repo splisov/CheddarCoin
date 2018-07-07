176cf67703580991628c4c9a13efc595e5c8dea0
splisov <sergey@splisov.com>
2018-07-07 19:40:52 -0400

    migrate functional

--- truffle-config.js
+++ truffle-config.js
@@ -11,8 +11,12 @@
  *     gasPrice: 10000000000,
  *   },
  */
-
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    }
+  }
 };
